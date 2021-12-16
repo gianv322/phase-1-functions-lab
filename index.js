05a86c5c100642ae8edd82cc8cd4fd54204273f9
Gianfranco Vaccaro <gianv322@gmail.com>
2021-12-16 14:21:40 -0500

    commit tree 9c6e96b4bafb92bb7dd4f833afcf7678805887d6

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(){
+    let scuberHQ = 42 
+    let pickUp = 50    
+        return pickUp - scuberHQ 
+    }
+ function distanceFromHqInFeet(){
+    let scuberHQ = 42 
+    let pickUp = 50    
+        return (pickUp - scuberHQ) * 264
+        }
